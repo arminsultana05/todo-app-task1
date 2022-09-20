db394ca6142c1f276439560ac35863936cf6f970
arminsultana <arminsultana556@gmail.com>
2022-09-20 13:01:47 +0600

    home page added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Table from '../Table/Table';
+
+const Home = () => {
+    return (
+        <div className="px-3 md:px-0">
+            <Table />
+        </div>
+    );
+};
+
+export default Home;
